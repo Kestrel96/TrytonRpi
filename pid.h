cf5af33b6c51fc9653df8127d87de60adefad7a9
Kestrel96 <kuba.poltorak@gmail.com>
2018-01-12 03:41:10 +0100

    3PIDv2

---
+++ pid.h
@@ -0,0 +1,36 @@
+#ifndef PID_H
+#define PID_H
+
+
+class PID
+{
+public:
+
+    PID();
+    void Compute(double pv, bool PD_on_big_e);
+    void Tuning(double kp, double Ti, double Td, double dt);
+    void Reset();
+    void Print();
+
+
+    double CV;
+
+private:
+
+    double kp;
+    double Ti;
+    double Td;
+    double dt;
+
+    double SP;
+    double e;
+    double last_e;
+    long double e_sum;
+
+    double P;
+    double I;
+    double D;
+
+};
+
+#endif // PID_H
