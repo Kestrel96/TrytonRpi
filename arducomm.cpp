9a560b29a629f951980a865a40281f0967d85272
Kestrel96 <kuba.poltorak@gmail.com>
2018-01-31 19:28:28 +0100

    asdasdsa

--- arducomm.cpp
+++ arducomm.cpp
@@ -2,8 +2,8 @@
 #include<iostream>
 #include<wiringSerial.h>
 
-#define MAX_PW 5200
-#define MIN_PW 520
+#define MAX_PW 2000
+#define MIN_PW 1050
 
 using namespace std;
 
@@ -17,13 +17,16 @@ void ArduComm::PrepareString(double CV1, double CV2){
 
     ArduString="A";
     int tmp=0;
+
     CV1=CV1/100 * MAX_PW;
+
     tmp=(int) CV1;
     if (tmp<=MIN_PW){
         tmp=MIN_PW;
     }
     ArduString+=to_string(tmp);
     ArduString+="B";
+
     CV2=CV2/100 * MAX_PW;
 
     tmp=(int) CV2;
@@ -33,5 +36,6 @@ void ArduComm::PrepareString(double CV1, double CV2){
 
     ArduString+=to_string(tmp);
     ArduString+='E';
+    ArduString+='\n';
 
 }
