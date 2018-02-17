20879e248763fe0440dd87ad5b68c1dfa0833039
Kestrel96 <kuba.poltorak@gmail.com>
2018-02-17 23:45:24 +0100

    sdas

--- arducomm.cpp
+++ arducomm.cpp
@@ -4,6 +4,8 @@
 
 #define MAX_PW 2000
 #define MIN_PW 1050
+#define MAX_CV 950
+#define MIN_CV 0
 
 using namespace std;
 
@@ -19,22 +21,22 @@ void ArduComm::PrepareString(double CV1, double CV2){
     int tmp=0;
 
     CV1=CV1 * (MAX_PW-MIN_PW);
-    CV1=CV1+MIN_PW;
+  //  CV1=CV1+MIN_PW;
 
     tmp=(int) CV1;
-    if (tmp<=MIN_PW){
-        tmp=MIN_PW;
+    if (tmp<=MIN_CV){
+        tmp=MIN_CV;
     }
     ArduString+=to_string(tmp);
     ArduString+="C";
 
     CV2=CV2 * (MAX_PW-MIN_PW);
-    CV2=CV2+MIN_PW;
+   // CV2=CV2+MIN_PW;
 
 
     tmp=(int) CV2;
-    if (tmp<=MIN_PW){
-        tmp=MIN_PW;
+    if (tmp<=MIN_CV){
+        tmp=MIN_CV;
     }
 
     ArduString+=to_string(tmp);
