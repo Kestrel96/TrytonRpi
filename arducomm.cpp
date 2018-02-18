ec896f406819fc0e05d46c66acf011ca644312a5
Kestrel96 <kuba.poltorak@gmail.com>
2018-02-18 02:38:00 +0100

    asdsa

--- arducomm.cpp
+++ arducomm.cpp
@@ -17,7 +17,13 @@ ArduComm::ArduComm()
 
 void ArduComm::PrepareString(double CV1, double CV2){
 
+    if(CV1>=0){
     ArduString="A";
+    }
+    else{
+        CV1=CV1*-1;
+        ArduString="B";
+    }
     int tmp=0;
 
     CV1=CV1/100 * (MAX_PW-MIN_PW);
@@ -27,8 +33,16 @@ void ArduComm::PrepareString(double CV1, double CV2){
     if (tmp<=MIN_CV){
         tmp=MIN_CV;
     }
+    if (tmp>=MAX_CV){
+        tmp=MAX_CV;
+    }
     ArduString+=to_string(tmp);
-    ArduString+="C";
+    if(CV2>=0){
+    ArduString+="C";}
+    else{
+        CV2=CV2*-1;
+        ArduString+="D";
+    }
 
     CV2=CV2/100 * (MAX_PW-MIN_PW);
    // CV2=CV2+MIN_PW;
@@ -38,9 +52,13 @@ void ArduComm::PrepareString(double CV1, double CV2){
     if (tmp<=MIN_CV){
         tmp=MIN_CV;
     }
+    if (tmp>=MAX_CV){
+        tmp=MAX_CV;
+    }
 
     ArduString+=to_string(tmp);
     ArduString+='E';
+    ArduString+='Z';
     ArduString+='\n';
 
 }
