5da5b496b3b4ad423d61cf61b487340f34af6dca
Kestrel96 <kuba.poltorak@gmail.com>
2018-04-27 09:05:38 +0200

    sdfd

--- main.cpp
+++ main.cpp
@@ -155,7 +155,7 @@ int main()
         ARD.PrepareString(Yaw_PID.CV,Pitch_PID.CV,Roll_PID.CV,Throttle_X,Throttle_Y,Throttle_Z);
         cout<<"String:"<<ARD.ArduString<<endl;
         serialFlush(SerialID);
-        sleep(1);
+        usleep(500);
         serialFlush(SerialID);
         serialPrintf(SerialID,ARD.ArduString.c_str());
 
